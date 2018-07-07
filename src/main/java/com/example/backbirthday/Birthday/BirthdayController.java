ddc30501bf3e9811fc8b87a9d9cfd9e9e7ca3643
CRHarding <casey.r.harding@gmail.com>
2018-07-07 17:28:25 -0400

    Setting up back end

--- src/main/java/com/example/backbirthday/Birthday/BirthdayController.java
+++ src/main/java/com/example/backbirthday/Birthday/BirthdayController.java
@@ -37,12 +37,12 @@ public class BirthdayController {
     public Birthday updateBirthdayById(@PathVariable Long birthdayId, @RequestBody Birthday birthdayRequest) {
         Birthday birthdayFromDb = birthdayRepository.findById(birthdayId).get();
 
-        birthdayFromDb.setBirthdayname(birthdayRequest.getBirthdayname());
-        birthdayFromDb.setFirstName(birthdayRequest.getFirstName());
-        birthdayFromDb.setLastName(birthdayRequest.getLastName());
-        birthdayFromDb.setEmail(birthdayRequest.getEmail());
-        birthdayFromDb.setAboutMe(birthdayRequest.getAboutMe());
-        birthdayFromDb.setPassword(birthdayRequest.getPassword());
+//        birthdayFromDb.setBirthdayname(birthdayRequest.getBirthdayname());
+//        birthdayFromDb.setFirstName(birthdayRequest.getFirstName());
+//        birthdayFromDb.setLastName(birthdayRequest.getLastName());
+//        birthdayFromDb.setEmail(birthdayRequest.getEmail());
+//        birthdayFromDb.setAboutMe(birthdayRequest.getAboutMe());
+//        birthdayFromDb.setPassword(birthdayRequest.getPassword());
 
         return birthdayRepository.save(birthdayFromDb);
     }
