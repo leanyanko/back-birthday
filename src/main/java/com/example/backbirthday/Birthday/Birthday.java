d16176d4408f6b708c58ca5b8d52b8cef7483299
CRHarding <casey.r.harding@gmail.com>
2018-07-07 17:52:55 -0400

    Adding birthdays to database

--- src/main/java/com/example/backbirthday/Birthday/Birthday.java
+++ src/main/java/com/example/backbirthday/Birthday/Birthday.java
@@ -1,10 +1,17 @@
 package com.example.backbirthday.Birthday;
 
 import com.example.backbirthday.User.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Set;
 
+@CrossOrigin
 @Data
 @AllArgsConstructor @NoArgsConstructor @Getter @Setter
 @Entity @Table(name = "BIRTHDAYS")
@@ -13,8 +20,11 @@ public class Birthday {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @OneToOne(mappedBy = "user", cascade=CascadeType.ALL)
-//    private User user = new User ();
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "CREATOR", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
 
     @Column(name = "WANTED_GIFTS")
     private String wantedGifts;
