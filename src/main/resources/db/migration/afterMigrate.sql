1d5377ee18c4d3a3236bf01d625615bbee29d3c7
CRHarding <casey.r.harding@gmail.com>
2018-07-07 16:00:24 -0400

    Initial database setup

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,14 +1,14 @@
 INSERT INTO USERS(USERNAME, PASSWORD, FIRST_NAME, LAST_NAME, EMAIL, ABOUT_ME)
 VALUES
     ('CRHarding', 'Casey', 'Casey', 'Harding', 'casey.r.harding@gmail.com', 'I like birthdays'),
-    ('AnnaAnna', 'Anna', 'L', 'Anna@anna.com', 'I love birthdays');
+    ('AnnaAnna', 'Anna', 'Anna', 'L', 'Anna@anna.com', 'I love birthdays');
+
+ INSERT INTO GIFTS (TITLE, DESCRIPTION, AMOUNT)
+VALUES
+    ('Car', 'This is a really cool car!', 50000),
+    ('Cake', 'All I want is cake...', 100);
 
 INSERT INTO BIRTHDAYS (CREATOR, WANTED_GIFTS)
 VALUES
     (1, 1),
     (2, 2);
-
-INSERT INTO GIFTS (TITLE, DESCRIPTION, AMOUNT)
-VALUES
-    ('Car', 'This is a really cool car!', 50000),
-    ('Cake', 'All I want is cake...', 100);
